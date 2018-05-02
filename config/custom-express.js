71d2b999ff051b1d52127fdd586f00cb3e8a25a2
fsrocha-dev <fsrocha.dev@gmail.com>
2018-05-02 00:04:18 -0300

    configurando git no vscode

--- config/custom-express.js
+++ config/custom-express.js
@@ -2,10 +2,14 @@
 var express = require('express');
 //Requisitando o Consign
 var consign = require('consign');
+//Requisitando o Body-parser
+var bodyParser = require('body-parser');
 
 module.exports = function(){	
 	var app = express();
 
+	app.use(bodyParser.urlencoded({extended: true}));
+	app.use(bodyParser.json());
 	//Padronizando as rotas com consign
 	consign()
 		.include('controllers')
--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -1,5 +1,11 @@
 module.exports = function(app){
-	app.get('/pagamentos', function(req, res){
-	res.send('Resposta pagamentos');
+	app.get('/pagamentos', function(req,res){
+		res.send('Resposta pagamentos');
+	});
+
+	app.post('/pagamentos/pagamento', function(req,res){
+		var pagamento = req.body;
+		console.log(pagamento);
+		res.send('ok');
 	});
 }
